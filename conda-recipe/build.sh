af8d199465d9512fc98918f1a8a55aa102f88231
Stephen Plaza <plazas@janelia.hhmi.org>
2017-08-31 10:01:04 -0400

    adds initial conda scripts

---
+++ conda-recipe/build.sh
@@ -0,0 +1,79 @@
+# Depending on our platform, shared libraries end with either .so or .dylib
+if [[ $(uname) == 'Darwin' ]]; then
+    DYLIB_EXT=dylib
+    CC=clang
+    CXX=clang++
+    CXXFLAGS="-I${PREFIX}/include -stdlib=libc++"
+else
+    DYLIB_EXT=so
+    CC=gcc
+    CXX=g++
+    CXXFLAGS="-I${PREFIX}/include"
+fi
+
+PY_VER=$(python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
+PY_ABIFLAGS=$(python -c "import sys; print('' if sys.version_info.major == 2 else sys.abiflags)")
+PY_ABI=${PY_VER}${PY_ABIFLAGS}
+
+BUILD_DIR=${BUILD_DIR-build}
+
+CONFIGURE_ONLY=0
+if [[ $1 != "" ]]; then
+    if [[ $1 == "--configure-only" ]]; then
+        CONFIGURE_ONLY=1
+    else
+        echo "Unknown argument: $1"
+        exit 1
+    fi
+fi
+
+
+# CONFIGURE
+mkdir -p "${BUILD_DIR}" # Using -p here is convenient for calling this script outside of conda.
+cd "${BUILD_DIR}"
+cmake ..\
+        -DCMAKE_C_COMPILER=${CC} \
+        -DCMAKE_CXX_COMPILER=${CXX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+        -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+        -DBOOST_ROOT="${PREFIX}" \
+        -DBoost_LIBRARY_DIR="${PREFIX}/lib" \
+        -DBoost_INCLUDE_DIR="${PREFIX}/include" \
+        -DCMAKE_MACOSX_RPATH=ON \
+        -DPYTHON_EXECUTABLE="${PYTHON}" \
+        -DPYTHON_LIBRARY="${PREFIX}/lib/libpython${PY_ABI}.${DYLIB_EXT}" \
+        -DPYTHON_INCLUDE_DIR="${PREFIX}/include/python${PY_ABI}" \
+##
+
+if [[ $CONFIGURE_ONLY == 0 ]]; then
+    # BUILD
+    make -j${CPU_COUNT}
+
+    # "install" to the build prefix (conda will relocate these files afterwards)
+    make install
+    
+    if [[ -z "$SKIP_TED_TESTS" || "$SKIP_TED_TESTS" == "0" ]]; then
+        echo "Running build tests.  To skip, set SKIP_TED_TESTS=1"
+    
+        # This script runs 'make test', which uses the build artifacts in the build directory, not the installed files.
+        # Therefore, they haven't been post-processed by conda to automatically locate their dependencies.
+        # We'll set LD_LIBRARY_PATH to avoid errors from ld
+        if [[ $(uname) == Darwin ]]; then
+            export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib":"${DYLD_FALLBACK_LIBRARY_PATH}"
+        else
+            export LD_LIBRARY_PATH="${PREFIX}/lib":"${LD_LIBRARY_PATH}"
+        fi
+
+        if ! make test; then
+            cat Testing/Temporary/LastTest.log
+            1>&2 echo "****************************************"
+            1>&2 echo "Post-build tests FAILED.  See log above."
+            1>&2 echo "****************************************"
+            exit 1
+        fi
+    fi
+fi
--- python/pyted.cpp
+++ python/pyted.cpp
@@ -1,101 +1,315 @@
-#include <boost/python.hpp>
-#include <boost/python/exception_translator.hpp>
-#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
-
+#include <boost/python/numeric.hpp> // TODO: needed?
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include <numpy/arrayobject.h>
+#include <util/ProgramOptions.h>
 #include <util/exceptions.h>
+#include <evaluation/VariationOfInformation.h>
+#include <evaluation/RandIndex.h>
+#include <evaluation/TolerantEditDistance.h>
 #include <git_sha1.h>
+#include "logging.h"
 #include "PyTed.h"
 
-template <typename Map, typename K, typename V>
-const V& genericGetter(const Map& map, const K& k) { return map[k]; }
-template <typename Map, typename K, typename V>
-void genericSetter(Map& map, const K& k, const V& value) { map[k] = value; }
+logger::LogChannel pytedlog("pytedlog", "[Ted] ");
 
-// iterator traits specializations
-//
-// if clang
-//   if clang < 6
-//      boost::detail
-//   else
-//      std
-// else if gcc
-//   if gcc < 5
-//      boost::detail
-//   else
-//      std
-// else
-//   std
-#if (defined __clang__ && __clang_major__ < 6) || (defined __GNUC__ && __GNUC__ < 5)
-namespace boost { namespace detail {
-#else
-namespace std {
-#endif
+PyTed::PyTed(const PyTed::Parameters& parameters) :
+		_parameters(parameters),
+		_numThreads(0) {
 
-#if (defined __clang__ && __clang_major__ < 6) || (defined __GNUC__ && __GNUC__ < 5)
-}} // namespace boost::detail
-#else
-} // namespace std
-#endif
+	switch (parameters.verbosity) {
 
-#if defined __clang__ && __clang_major__ < 6
-// std::shared_ptr support
-	template<class T> T* get_pointer(std::shared_ptr<T> p){ return p.get(); }
-#endif
+	case 0:
+		pyted::setLogLevel(logger::Quiet);
+		break;
+	case 1:
+		pyted::setLogLevel(logger::Error);
+		break;
+	case 2:
+		pyted::setLogLevel(logger::User);
+		break;
+	case 3:
+		pyted::setLogLevel(logger::Debug);
+		break;
+	default:
+		pyted::setLogLevel(logger::All);
+		break;
+	}
 
-namespace pyted {
-
-/**
- * Translates an Exception into a python exception.
- *
- **/
-void translateException(const Exception& e) {
-
-	if (boost::get_error_info<error_message>(e))
-		PyErr_SetString(PyExc_RuntimeError, boost::get_error_info<error_message>(e)->c_str());
-	else
-		PyErr_SetString(PyExc_RuntimeError, e.what());
+	LOG_DEBUG(pytedlog) << "constructed" << std::endl;
+	initialize();
 }
 
-std::string
-getVersion() {
+void
+PyTed::setNumThreads(int numThreads) {
 
-	return __git_sha1;
+	_numThreads = numThreads;
 }
 
-/**
- * Defines all the python classes in the module libpyted. Here we decide 
- * which functions and data members we wish to expose.
- */
-BOOST_PYTHON_MODULE(pyted) {
+boost::python::dict
+PyTed::createReport(PyObject* gt, PyObject* rec, PyObject* voxel_size, PyObject* corrected) {
 
-	boost::python::register_exception_translator<Exception>(&translateException);
+	util::ProgramOptions::setOptionValue("numThreads", util::to_string(_numThreads));
 
-	boost::python::def("version", getVersion);
+	boost::python::dict summary;
 
-	boost::python::numeric::array::set_module_and_type("numpy", "ndarray");
+	ImageStack groundTruth = imageStackFromArray(gt, voxel_size);
+	ImageStack reconstruction = imageStackFromArray(rec, voxel_size);
 
-	boost::python::class_<PyTed::Parameters>("Parameters")
-			.def_readwrite("report_ted", &PyTed::Parameters::reportTed)
-			.def_readwrite("report_rand", &PyTed::Parameters::reportRand)
-			.def_readwrite("report_voi", &PyTed::Parameters::reportVoi)
-			.def_readwrite("report_detection_overlap", &PyTed::Parameters::reportDetectionOverlap)
-			.def_readwrite("ignore_background", &PyTed::Parameters::ignoreBackground)
-			.def_readwrite("from_skeleton", &PyTed::Parameters::fromSkeleton)
-			.def_readwrite("distance_threshold", &PyTed::Parameters::distanceThreshold)
-			.def_readwrite("gt_background_label", &PyTed::Parameters::gtBackgroundLabel)
-			.def_readwrite("rec_background_label", &PyTed::Parameters::recBackgroundLabel)
-			.def_readwrite("have_background", &PyTed::Parameters::haveBackground)
-			.def_readwrite("ted_timeout", &PyTed::Parameters::tedTimeout)
-			.def_readwrite("report_ted_error_locations", &PyTed::Parameters::reportTedErrorLocations)
-			.def_readwrite("verbosity", &PyTed::Parameters::verbosity)
-			;
+	if (_parameters.reportVoi) {
 
-	boost::python::class_<PyTed>("Ted")
-			.def(boost::python::init<PyTed::Parameters>())
-			.def("set_num_threads", &PyTed::setNumThreads)
-			.def("create_report", (boost::python::dict(PyTed::*)(PyObject*,PyObject*,PyObject*))(&PyTed::createReport))
-			.def("create_report", (boost::python::dict(PyTed::*)(PyObject*,PyObject*,PyObject*,PyObject*))(&PyTed::createReport))
-			;
+		VariationOfInformation voi(_parameters.ignoreBackground);
+		VariationOfInformationErrors errors = voi.compute(groundTruth, reconstruction);
+
+		summary["voi_split"] = errors.getSplitEntropy();
+		summary["voi_merge"] = errors.getMergeEntropy();
+	}
+
+	if (_parameters.reportRand) {
+
+		RandIndex rand(_parameters.ignoreBackground);
+		RandIndexErrors errors = rand.compute(groundTruth, reconstruction);
+
+		summary["rand_index"] = errors.getRandIndex();
+		summary["rand_precision"] = errors.getPrecision();
+		summary["rand_recall"] = errors.getRecall();
+		summary["adapted_rand_error"] = errors.getAdaptedRandError();
+	}
+
+	if (_parameters.reportTed) {
+
+		TolerantEditDistance::Parameters tedParameters;
+		tedParameters.fromSkeleton = _parameters.fromSkeleton;
+		tedParameters.distanceThreshold = _parameters.distanceThreshold;
+		tedParameters.reportFPsFNs = _parameters.haveBackground;
+		tedParameters.allowBackgroundAppearance = true; // to be backwards compatible, might change at some point
+		tedParameters.gtBackgroundLabel = _parameters.gtBackgroundLabel;
+		tedParameters.recBackgroundLabel = _parameters.recBackgroundLabel;
+		tedParameters.timeout = _parameters.tedTimeout;
+
+		TolerantEditDistance ted(tedParameters);
+		TolerantEditDistanceErrors errors = ted.compute(groundTruth, reconstruction);
+
+		boost::python::dict splits;
+		for (size_t split_label : errors.getSplitLabels()) {
+
+			boost::python::list split_into;
+			for (size_t into : errors.getSplits(split_label))
+				split_into.append(into);
+			splits[split_label] = split_into;
+		}
+
+		boost::python::dict merges;
+		for (size_t merge_label : errors.getMergeLabels()) {
+
+			boost::python::list merge_into;
+			for (size_t into : errors.getMerges(merge_label))
+				merge_into.append(into);
+			merges[merge_label] = merge_into;
+		}
+
+		boost::python::list fps;
+		if (tedParameters.reportFPsFNs)
+			for (size_t l : errors.getFalsePositives())
+				fps.append(l);
+		boost::python::list fns;
+		if (tedParameters.reportFPsFNs)
+			for (size_t l : errors.getFalseNegatives())
+				fns.append(l);
+
+		boost::python::list matches;
+		for (const TolerantEditDistanceErrors::Match& match : errors.getMatches())
+			matches.append(
+					boost::python::make_tuple(
+							match.gtLabel,
+							match.recLabel,
+							match.overlap));
+
+		if (_parameters.reportTedErrorLocations) {
+
+			boost::python::list splitErrors;
+			for (const TolerantEditDistanceErrors::SplitError& splitError : errors.getSplitErrors()) {
+
+				boost::python::dict split_error;
+				split_error["gt_label"] = splitError.gtLabel;
+				split_error["rec_label_1"] = splitError.recLabel1;
+				split_error["rec_label_2"] = splitError.recLabel2;
+				split_error["distance"] = splitError.distance;
+				split_error["location"] = boost::python::make_tuple(
+						splitError.location.z*groundTruth.getResolutionZ(),
+						splitError.location.y*groundTruth.getResolutionY(),
+						splitError.location.x*groundTruth.getResolutionX());
+				split_error["size"] = splitError.size;
+
+				splitErrors.append(split_error);
+			}
+
+			boost::python::list mergeErrors;
+			for (const TolerantEditDistanceErrors::MergeError& mergeError : errors.getMergeErrors()) {
+
+				boost::python::dict merge_error;
+				merge_error["rec_label"] = mergeError.recLabel;
+				merge_error["gt_label_1"] = mergeError.gtLabel1;
+				merge_error["gt_label_2"] = mergeError.gtLabel2;
+				merge_error["distance"] = mergeError.distance;
+				merge_error["location"] = boost::python::make_tuple(
+						mergeError.location.z*groundTruth.getResolutionZ(),
+						mergeError.location.y*groundTruth.getResolutionY(),
+						mergeError.location.x*groundTruth.getResolutionX());
+				merge_error["size"] = mergeError.size;
+
+				mergeErrors.append(merge_error);
+			}
+
+			summary["split_errors"] = splitErrors;
+			summary["merge_errors"] = mergeErrors;
+		}
+
+		summary["ted_split"] = errors.getNumSplits();
+		summary["ted_merge"] = errors.getNumMerges();
+		summary["splits"] = splits;
+		summary["merges"] = merges;
+		summary["matches"] = matches;
+		if (tedParameters.reportFPsFNs) {
+			summary["ted_fp"] = errors.getNumFalsePositives();
+			summary["ted_fn"] = errors.getNumFalseNegatives();
+			summary["fps"] = fps;
+			summary["fns"] = fns;
+		}
+		summary["ted_inference_time"] = errors.getInferenceTime();
+		summary["ted_num_variables"] = errors.getNumVariables();
+
+		if (corrected != 0)
+			imageStackToArray(ted.getCorrectedReconstruction(), corrected);
+	}
+
+	summary["ted_version"] = std::string(__git_sha1);
+
+	return summary;
 }
 
-} // namespace pyted
+ImageStack
+PyTed::imageStackFromArray(PyObject* a, PyObject* voxel_size) {
+
+	// create (expect?) a C contiguous uint32 array
+	PyArray_Descr* descr = PyArray_DescrFromType(NPY_UINT32);
+	PyArrayObject* array = (PyArrayObject*)(PyArray_FromAny(a, descr, 2, 3, 0, NULL));
+
+	PyArray_Descr* vs_descr = PyArray_DescrFromType(NPY_FLOAT64);
+	PyArrayObject* vs = (PyArrayObject*)(PyArray_FromAny(voxel_size, vs_descr, 1, 1, 0, NULL));
+
+	if (vs == NULL)
+		UTIL_THROW_EXCEPTION(
+				UsageError,
+				"only voxel size arrays of dimension 1, with datatype np.float64 are supported");
+
+	if (array == NULL)
+		UTIL_THROW_EXCEPTION(
+				UsageError,
+				"only arrays of dimension 2 or 3, with datatype np.uint32 are supported");
+
+	LOG_DEBUG(pytedlog) << "converted to array" << std::endl;
+
+	int dims = PyArray_NDIM(array);
+	size_t width, height, depth;
+
+	if (dims == 2) {
+
+		depth = 1;
+		height = PyArray_DIM(array, 0);
+		width = PyArray_DIM(array, 1);
+
+	} else {
+
+		depth =  PyArray_DIM(array, 0);
+		height = PyArray_DIM(array, 1);
+		width = PyArray_DIM(array, 2);
+	}
+
+	LOG_DEBUG(pytedlog) << "copying data..." << std::endl;
+
+	ImageStack stack;
+
+	double res_x = *static_cast<double*>(PyArray_GETPTR1(vs, 2));
+	double res_y = *static_cast<double*>(PyArray_GETPTR1(vs, 1));
+	double res_z = *static_cast<double*>(PyArray_GETPTR1(vs, 0));
+
+	stack.setResolution(res_x, res_y, res_z);
+
+	for (size_t z = 0; z < depth; z++) {
+
+		std::shared_ptr<Image> image = std::make_shared<Image>(width, height);
+		for (size_t y = 0; y < height; y++)
+			for (size_t x = 0; x < width; x++) {
+
+				uint32_t value;
+				if (dims == 2)
+					value = *static_cast<uint32_t*>(PyArray_GETPTR2(array, y, x));
+				else
+					value = *static_cast<uint32_t*>(PyArray_GETPTR3(array, z, y, x));
+
+				(*image)(x,y) = value;
+			}
+
+		stack.add(image);
+	}
+
+	LOG_DEBUG(pytedlog) << "done" << std::endl;
+
+	return stack;
+}
+
+void
+PyTed::imageStackToArray(const ImageStack& stack, PyObject* a) {
+
+	// create (expect?) a C contiguous uint32 array
+	PyArray_Descr* descr = PyArray_DescrFromType(NPY_UINT32);
+	PyArrayObject* array = (PyArrayObject*)(PyArray_FromAny(a, descr, 2, 3, 0, NULL));
+
+	if (array == NULL)
+		UTIL_THROW_EXCEPTION(
+				UsageError,
+				"only arrays of dimension 2 or 3, with datatype np.uint32 are supported");
+
+	LOG_DEBUG(pytedlog) << "converted to array" << std::endl;
+
+	int dims = PyArray_NDIM(array);
+	size_t width, height, depth;
+
+	if (dims == 2) {
+
+		depth = 1;
+		height = PyArray_DIM(array, 0);
+		width = PyArray_DIM(array, 1);
+
+	} else {
+
+		depth =  PyArray_DIM(array, 0);
+		height = PyArray_DIM(array, 1);
+		width = PyArray_DIM(array, 2);
+	}
+
+	LOG_DEBUG(pytedlog) << "copying data..." << std::endl;
+
+	for (size_t z = 0; z < depth; z++) {
+		for (size_t y = 0; y < height; y++)
+			for (size_t x = 0; x < width; x++) {
+
+				uint32_t value = (*stack[z])(x,y);
+				if (dims == 2)
+					*static_cast<uint32_t*>(PyArray_GETPTR2(array, y, x)) = value;
+				else
+					*static_cast<uint32_t*>(PyArray_GETPTR3(array, z, y, x)) = value;
+			}
+	}
+
+	LOG_DEBUG(pytedlog) << "done" << std::endl;
+}
+
+void
+PyTed::initialize() {
+
+	// import_array is a macro expanding to returning different types across 
+	// different versions of the NumPy API. This lambda hack works around that.
+	auto a = []{ import_array(); };
+	a();
+}
